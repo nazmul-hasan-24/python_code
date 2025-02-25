9ee5ef4d8a5ac45865031e12338fb9404a9962f2
Nazmul Hasan <nazmul.tex99@gmail.com>
2025-02-25 21:51:24 +0500

    2nd day

---
+++ Basic/var_names.py
@@ -0,0 +1,19 @@
+
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Multi Words Variable Names
+
+#Camel Case
+myVariableName = "John"
+
+#Pascal Case
+MyVarName = "Hasan"
+
+#Snake Case
+my_var_name = "hasan"
--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import sys
 print(sys.version)
 if 5 > 2:
     print("True res")
+    # error for indentation
+    #if 5 > 4:
+    #print("Error")
