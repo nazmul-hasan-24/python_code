16ab16606cd5e4e475dcdc02c39970b906a43d1c
Nazmul Hasan <nazmul.tex99@gmail.com>
2025-03-01 21:32:31 +0500

    Day 6

---
+++ Basic/strings/modify_string.py
@@ -0,0 +1,14 @@
+#Upper case
+name = "Nazmul Hasan"
+print(name.upper())
+
+#Lower case
+print(name.lower())
+
+#Remove whitespace
+print(name.strip())
+
+#Replace String
+print(name.replace("Hasan", "Papon"))
+
+#split string
---
+++ Basic/strings/slicing_string.py
@@ -0,0 +1,14 @@
+#Slicing
+#we can return a range of characters by using the slice syntax.
+b = "A quick brown fox!"
+print(b[2:5])
+
+#Slice From the Start
+print(b[:7])
+
+# Slice to the end
+print(b[1:])
+
+# Negative Indexing
+#Use negative indexes to start the slice from the end of the string:
+print(b[-5:-2])
