5f2e5e92dd839c7c261c8f5f03fa1a423d9a71e4
Nazmul Hasan <nazmul.tex99@gmail.com>
2025-03-07 11:17:04 +0600

    Day 9

---
+++ operators/arithmetic.py
@@ -0,0 +1,35 @@
+#Operators are used to perform operations on variables and values.
+mango_price = 20
+banana_price = 8
+total_price = mango_price + banana_price
+print("Total price:", total_price , "Tk")
+
+""" 
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+"""
+# Arithmetic operators
+"""
+Addition +
+Substurcion -
+Multiplication *
+Divition /
+Modules % Give reminder
+Exponentiation ** 2nd number power of 1st number 
+the floor division // rounds the result down to the nearest whole number
+"""
+rool = 4
+rool2 = 5
+print(rool // rool2)
+print(rool ** rool2)
+
+
+
+
---
+++ operators/assignment_opt.py
@@ -0,0 +1,21 @@
+#Assignment operators
+#Assignment operators are used to assign values to variables:
+# Example   #Same as
+"""
+Operator	Example	   Same As	
+    =	    x = 5	    x = 5	
+    +=	    x += 3	    x = x + 3	
+    -=	    x -= 3	    x = x - 3	
+    *=	    x *= 3	    x = x * 3	
+    /=	    x /= 3	    x = x / 3	
+    %=	    x %= 3	    x = x % 3	
+    //=	    x //= 3	    x = x // 3	
+    **=	    x **= 3	    x = x ** 3	
+    &=	    x &= 3	    x = x & 3  Perform Bitwise AND	
+    |=	    x |= 3	    x = x | 3  bitwise OR operation
+    ^=	    x ^= 3	    x = x ^ 3   Bitwise XOR
+    >>=	    x >>= 3	    x = x >> 3	 Right Shift
+    <<=	    x <<= 3	    x = x << 3	Left shift
+    :=	    print(x := 3)	x = 3   Assign value x = 3
+    print(x)
+"""
